5deecdf2711a5427684deda7f3f18bcc9c419985
ghaciosm <haciosmanoglugulcin@gmail.com>
2024-10-13 16:00:02 +0300

    initial frontend server

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Merhaba, TypeScript ile React Projem!</h1>
+    </div>
+  );
+}
+
+export default App;
+
