4823d93e1f6ef44d1e8237eee2c4ae91b8d52ccd
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-10-14 13:43:19 +0300

    Basic static Login Page

---
+++ front/src/pages/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css'; // Stil dosyasını eklemeyi unutma!!
+
+const ForgotPassword: React.FC = () => {
+  return (
+    <div>Şifremi unuttum</div>
+  );
+}
+
+export default ForgotPassword;
