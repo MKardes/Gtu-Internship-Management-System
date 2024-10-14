b0bd3e371d84ae85a13bbb2830364e0cbd86e999
POYRAZ5342 <ahmetbahacepni5335@gmail.com>
2024-10-14 15:56:55 +0300

    dir_tree

---
+++ backend/src/routes/services.ts
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 
+ * // src/routes/userRoutes.ts
+import express from 'express';
+import * as UserController from '../controllers/userController';
+
+const router = express.Router();
+
+router.get('/users', UserController.getUsers);
+router.post('/users', UserController.createUser);
+
+export default router;
+
+ */
