ea406bad4c1468eb4af507646b2f2b1d61b7961c
Musab Kardeş <musabkardes@gmail.com>
2024-11-02 21:58:19 +0300

    tokens updated

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -11,5 +11,5 @@ export const POSTGRESQL_USER = process.env.POSTGRESQL_USER;
 export const POSTGRESQL_PWD = process.env.POSTGRESQL_PWD;
 export const MIGRATE_DB = process.env.MIGRATE_DB === 'true';
 
-export const ACCESS_TOKEN_SECRET = 'your_access_token_secret';
-export const REFRESH_TOKEN_SECRET = 'your_refresh_token_secret';
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
