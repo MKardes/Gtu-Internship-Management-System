be883fcfd70a8d6f50c13da73afde2c2455c3739
Musab Kardeş <musabkardes@gmail.com>
2024-11-02 21:56:02 +0300

    .env added for backend

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -6,15 +6,23 @@ import { Mentor } from './src/entities/mentor.entitiy';
 import { Student } from './src/entities/student.entitiy';
 import { User } from './src/entities/user.entity';
 import { Department } from './src/entities/department.entity';
+import { 
+  MIGRATE_DB,
+  POSTGRESQL_DB,
+  POSTGRESQL_HOST,
+  POSTGRESQL_PORT,
+  POSTGRESQL_USER,
+  POSTGRESQL_PWD,
+ } from './src/config';
 
 export const AppDataSource = new DataSource({
   type: 'postgres', // or your database type (e.g., 'postgres')
-  host: 'musabkardes.com.tr',
-  port: 5555,
-  username: 'interndbuser',
-  password: 'interndbpass',
-  database: 'interndb',
-  synchronize: true,
+  host: POSTGRESQL_HOST,
+  port: POSTGRESQL_PORT,
+  username: POSTGRESQL_USER,
+  password: POSTGRESQL_PWD,
+  database: POSTGRESQL_DB,
+  synchronize: MIGRATE_DB,
   logging: false,
   entities: [Company, File, Internship, Mentor, Student, User, Department],
   migrations: [],
