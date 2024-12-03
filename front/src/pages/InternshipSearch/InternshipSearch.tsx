aae9ad772b3564eed03da723da90ee19a9d9c9cd
Cemal BOLAT <103999323+CemBOLAT@users.noreply.github.com>
2024-12-03 19:38:35 +0300

    navbar and new pages
    navbar son haline geldi.
    yeni sayfalar için template eklendi.
    department admindeki hata çözüldü

---
+++ front/src/pages/InternshipSearch/InternshipSearch.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"
+import "./InternshipSearch.css";
+
+const InternshipSearch: React.FC = () => {
+    return <div>Internship Search</div>;
+};
+
+export default InternshipSearch;
---
+++ front/src/pages/Report/Report.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"
+import "./Report.css";
+
+const Report: React.FC = () => {
+    return <div>My Reports</div>;
+};
+
+export default Report;
