ce33902c39f3c37b2d346060489801a1f0a08dfa
Ömer Olkay <oolkay@gmail.com>
2025-01-02 15:49:14 +0300

    bugfix, docx report

--- backend/src/utils/ResponseHandler.ts
+++ backend/src/utils/ResponseHandler.ts
@@ -24,3 +24,22 @@ export const logRequest = (
     // Yanıtı gönderme
     res.status(result.status).json(result.data);
 };
+
+export const logBufferRequest = (
+    res: Response, // request objesini parametre olarak alıyoruz
+    result: { status: number; data: any; headers: any },
+    endpoint: string,
+    req: Request,
+) => {
+    let myRequest: MyRequest = req as MyRequest;
+
+    // Yanıt durumuna göre loglama
+    if (result.status >= 200 && result.status < 300) {
+        logger.log(`${endpoint} - Success: Returned ${Array.isArray(result.data) ? result.data.length : 1} items, User Id: ${JSON.stringify(myRequest.user?.id)}`);
+    } else {
+        logger.log(`${endpoint} - Failure: Status ${result.status}, Data: ${JSON.stringify(result.data)}`);
+    }
+
+    // Yanıtı gönderme
+    res.status(result.status).header(result.headers).send(result.data);
+};
