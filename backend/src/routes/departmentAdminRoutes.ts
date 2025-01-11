b5029ff72cc700364017b1cdaff214cf6a4575a5
Cemal BOLAT <cemalbolat2003@outlook.com>
2025-01-11 15:55:34 +0300

    department-awareness+UIFİx
    Butonun genişliği düzenlendi.
    Departmandakiler sadece kendi departmanındaki 'userları' görüyor.
    
    Rapor oluştururken ise kullanıcı kendi departmanındaki tüm kullanıcıları görüyor.
    
    Yeni endpoint yazdım. including admins adında.

--- backend/src/routes/departmentAdminRoutes.ts
+++ backend/src/routes/departmentAdminRoutes.ts
@@ -5,6 +5,7 @@ import departmentAdminController from '../controllers/departmentAdminController'
 const router = express.Router();
 
 router.get('/department-admin/users', verifyToken, departmentAdminController.getAllUsers);
+router.get('/department-admin/all-users', verifyToken, departmentAdminController.getAllUsersIncludingAdmins);
 router.get('/department-admin/department-admin', verifyToken, departmentAdminController.getDepartmentAdmin);
 router.post('/department-admin/create-user', verifyToken, departmentAdminController.createUser);
 
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -43,7 +43,7 @@ const App: React.FC = () => {
           <Route 
             path = "/dashboard"
             element = {
-              <PrivateRoute element = {<Dashboard />} requiredRole = "DepartmentAdmin" />
+              <PrivateRoute element = {<Dashboard />} requiredRole = "DepartmentAdmin||User" />
             }
           />
           <Route 
